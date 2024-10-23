202434b844a071850b97ad8406ced86ddb569621
VALLARASU G <144762287+VALLARASU64@users.noreply.github.com>
2024-10-23 22:02:58 +0530

    Add files via upload

---
+++ utils/data/educations.js
@@ -0,0 +1,20 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Bachelor of Technology",
+    duration: "2022-2026",
+    College: "Bharathidasan Engineering College",
+  },
+  {
+    id: 2,
+    title: "Higher Secondary",
+    duration: "2020 - 2022",
+    School: "Vaani Matric Higher secondary school",
+  },
+  {
+    id: 3,
+    title: "Secondary School Certificate",
+    duration: "2010 - 2020",
+    School: "Vaani Matric Higher secondary school",
+  }
+]
---
+++ utils/data/experience.js
@@ -0,0 +1,9 @@
+export const experiences = [
+
+  {
+    id: 1,
+    title: "FullStack Developer",
+    company: "SELF lEARNING",
+    duration: "2023-2024"
+  }
+]
